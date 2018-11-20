e78123ececfd53f15de47bea1132efe88e791645
Muabid <bruno2698@gmail.com>
2018-11-20 18:08:07 -0300

    lol

--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -1,19 +1,21 @@
 package controllers;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-
-
+import model.Pokemon;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
+import utils.PokeRepo;
 
 public class HomeController {
 
 	public static ModelAndView index(Request req, Response res) {
 		Map<String,Object> view=new HashMap<>();
-		
+		List<Pokemon> pokemons= new PokeRepo().searchBySubName("pika");
+		view.put("pokemons",pokemons);
 		return new ModelAndView(view, "index.hbs");
 	}
 }
--- src/main/java/model/Pokemon.java
+++ src/main/java/model/Pokemon.java
@@ -26,9 +26,16 @@ public class Pokemon {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	public static void main(String []argv) {
+		String str="https://pokeapi.co/api/v2/pokemon/10080/";
+		Pokemon p=new Pokemon("Pikca",str);
+		System.out.println(p.getPic());
+	}
 
 	public String getId() {
-		return url.substring(url.length()-3, url.length()-1);
+		String str=url.substring(0, url.length()-1);
+		return str.substring(str.lastIndexOf("/")+1);
 	}
 	
 	public String getPic() {
